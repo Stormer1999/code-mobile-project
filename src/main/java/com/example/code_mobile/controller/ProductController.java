2beef9f98fee0de580b9e132e782a7998a3427ff
Worabordin Chumsintaiphat <stormer@Worabordins-MacBook-Air.local>
2022-01-03 13:17:41 +0700

    HTTP Post Method

--- src/main/java/com/example/code_mobile/controller/ProductController.java
+++ src/main/java/com/example/code_mobile/controller/ProductController.java
@@ -1,18 +1,41 @@
 package com.example.code_mobile.controller;
 
+import com.example.code_mobile.model.Product;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
+    private final AtomicLong counter = new AtomicLong();
+    private final List<Product> products = new ArrayList<>();
+
+
     @GetMapping()
-    public String getProducts(){
-        return "all product";
+    public List<Product> getProducts(){
+        return products;
     }
 
     @GetMapping("/{id}")
-    public String getProduct(@PathVariable long id){
-        return "product id: " + id ;
+    public Product getProduct(@PathVariable long id){
+        return products.stream()
+                .filter(result -> result.getId() == id)
+                .findFirst().orElseThrow();
+    }
+
+    @PostMapping()
+    public Product addProduct(@RequestBody Product product){
+        Product data = new Product(counter.incrementAndGet(),
+                product.getName(),
+                product.getImage(),
+                product.getPrice(),
+                product.getStock()
+                );
+        products.add(data);
+        return data;
     }
 }
