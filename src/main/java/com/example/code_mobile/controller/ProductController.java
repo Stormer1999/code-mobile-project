a425ea6002d8a32e581d378dca405a1d0afa4c15
Worabordin Chumsintaiphat <stormer@Worabordins-MacBook-Air.local>
2022-02-17 10:11:09 +0700

    reformat code to google format

--- src/main/java/com/example/code_mobile/controller/ProductController.java
+++ src/main/java/com/example/code_mobile/controller/ProductController.java
@@ -11,49 +11,46 @@ import java.util.concurrent.atomic.AtomicLong;
 @RequestMapping("/product")
 public class ProductController {
 
-    private final AtomicLong counter = new AtomicLong();
-    private final List<Product> products = new ArrayList<>();
+  private final AtomicLong counter = new AtomicLong();
+  private final List<Product> products = new ArrayList<>();
 
+  @GetMapping()
+  public List<Product> getProducts() {
+    return products;
+  }
 
-    @GetMapping()
-    public List<Product> getProducts(){
-        return products;
-    }
+  @GetMapping("/{id}")
+  public Product getProduct(@PathVariable long id) {
+    return products.stream().filter(result -> result.getId() == id).findFirst().orElseThrow();
+  }
 
-    @GetMapping("/{id}")
-    public Product getProduct(@PathVariable long id){
-        return products.stream()
-                .filter(result -> result.getId() == id)
-                .findFirst().orElseThrow();
-    }
+  @PostMapping()
+  public Product addProduct(@RequestBody Product product) {
+    Product data =
+        new Product(
+            counter.incrementAndGet(),
+            product.getName(),
+            product.getImage(),
+            product.getPrice(),
+            product.getStock());
+    products.add(data);
+    return data;
+  }
 
-    @PostMapping()
-    public Product addProduct(@RequestBody Product product){
-        Product data = new Product(counter.incrementAndGet(),
-                product.getName(),
-                product.getImage(),
-                product.getPrice(),
-                product.getStock()
-                );
-        products.add(data);
-        return data;
-    }
-
-    @PutMapping("/{id}")
-    public void editProduct(
-            @RequestBody Product product,
-            @PathVariable long id
-    ){
-        products.stream()
-                .filter(result -> result.getId() == id)
-                .findFirst()
-                .ifPresentOrElse(result -> {
-                    result.setName(product.getName());
-                    result.setImage(product.getImage());
-                    result.setPrice(product.getPrice());
-                    result.setStock(product.getStock());
-                }, () -> {
-                    //todo
-                });
-    }
+  @PutMapping("/{id}")
+  public void editProduct(@RequestBody Product product, @PathVariable long id) {
+    products.stream()
+        .filter(result -> result.getId() == id)
+        .findFirst()
+        .ifPresentOrElse(
+            result -> {
+              result.setName(product.getName());
+              result.setImage(product.getImage());
+              result.setPrice(product.getPrice());
+              result.setStock(product.getStock());
+            },
+            () -> {
+              // todo
+            });
+  }
 }
--- src/main/java/com/example/code_mobile/util/DateUtils.java
+++ src/main/java/com/example/code_mobile/util/DateUtils.java
@@ -10,27 +10,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-//@Configuration
-//@Scope("prototype")
+// @Configuration
+// @Scope("prototype")
 public class DateUtils {
-    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+  private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
 
-    public DateUtils() {
-        System.out.println("object created");
-    }
+  public DateUtils() {
+    System.out.println("object created");
+  }
 
-    @Bean
-    public String todayString() {
-        return simpleDateFormat.format(new Date());
-    }
+  @Bean
+  public String todayString() {
+    return simpleDateFormat.format(new Date());
+  }
 
-    @PostConstruct
-    public void init() throws Exception {
-        System.out.println("object init");
-    }
+  @PostConstruct
+  public void init() throws Exception {
+    System.out.println("object init");
+  }
 
-    @PreDestroy
-    public void destroy() throws Exception {
-        System.out.println("object destroy");
-    }
+  @PreDestroy
+  public void destroy() throws Exception {
+    System.out.println("object destroy");
+  }
 }
